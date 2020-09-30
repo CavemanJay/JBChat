77d1aec86eb2d2aa9a860684e318a12405b13ecc
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2020-09-30 00:48:25 -0400

    Update dependencies and build scripts/tools

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+export type Socket = {
+  port: number;
+  ipAddress: string;
+};
+
+import express from "express";
+import { createServer } from "http";
+
+const app = express();
+const http = createServer(app);
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+const port = Number.parseInt(process.env.PORT ?? "") || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+let connection: Socket = { ipAddress: "127.0.0.1", port: 80 };
+
+function connectToServer(cnn: { src: Socket; dest: Socket }) {}
