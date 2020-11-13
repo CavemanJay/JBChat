4b587393d16514bd9a29f7922e0479b363674a96
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2020-11-13 00:20:11 -0500

    Added basic joining logic

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -11,7 +11,14 @@ import http from "http";
 
 import socketIO from "socket.io";
 
-const rooms: Room[] = [];
+const rooms: Room[] = [
+  { id: "General", messages: ["Hi! Welcome to JB Chat!"] },
+];
+
+function createRoom(id: string) {
+  rooms.push({ id, messages: [] });
+}
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server, { path: "/chat" });
@@ -61,7 +68,33 @@ app.post("/messages", (req, res) => {
 // });
 
 io.on("connection", (socket) => {
-  console.log(socket);
+  const { id, room } = socket.handshake.query;
+
+  const targetRoom = rooms.find((x) => x.id === room);
+
+  socket.on("roomNotFoundResponse", (response: string) => {
+    if (response.toLowerCase().startsWith("y")) {
+      console.log(`Creating room ${room}`);
+
+      createRoom(room);
+      socket.join(room);
+    } else {
+      socket.disconnect();
+    }
+  });
+
+  if (!targetRoom) {
+    console.log(
+      `Received connection from user ${id} to room ${room}. Room nonexistent.`
+    );
+
+    socket.emit(
+      "roomNotFound",
+      `The requested room '${room}' does not exist, do you wish to create it? `
+    );
+
+    // socket.disconnect();
+  }
 });
 
 server.listen(port, () => {
--- TS Client/src/client.ts
+++ TS Client/src/client.ts
@@ -1,6 +1,18 @@
 import io from "socket.io-client";
+import promptSync from "prompt-sync";
+const prompt = promptSync();
 
-const socket = io("http://localhost:3000", { path: "/chat", query: "Test" });
+const socket = io("http://localhost:3000", {
+  path: "/chat",
+  query: { id: "Jay", room: "Generala" },
+});
 
 socket.connect();
-socket.send("hello");
+
+socket.on("roomNotFound", (message: string) => {
+  //   console.log(message);
+
+  const response = prompt(message);
+  socket.emit("roomNotFoundResponse", response);
+});
+// socket.disconnect();
