f070839e71fc1a7a55e2bad8440c6b38b63f01f7
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2020-12-14 17:25:11 -0500

    Added in some basic frontend code Created initial socket on app start Need to configure event listeners

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,38 @@
 import io from "socket.io-client";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [socket, setSocket] = useState<SocketIOClient.Socket>();
+  const [message, setMessage] = useState("");
+
+  // Code to run on initialization
+  useEffect(() => {
+    const _socket = io("http://localhost:3000", {
+      path: "/chat",
+      query: { id: "Jay", room: "General" },
+    });
+    setSocket(_socket);
+  }, []);
+
+  const changeMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  };
+
+  const sendMessage = () => {};
+
   return (
     <div className="App">
+      <p>{JSON.stringify({ message })}</p>
+      <hr />
       <div id="message-container">
         <form id="message-form">
-          <input type="text" id="message-input" />
-          <button type="submit" id="send-button">
+          <input
+            type="text"
+            id="message-input"
+            value={message}
+            onChange={changeMessage}
+          />
+          <button type="submit" id="send-button" onClick={sendMessage}>
             Send Message
           </button>
         </form>
